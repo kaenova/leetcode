8e930d05b0dbc28b94e3be8ddc922072a4a24b79
Kaenova Mahendra Auditama <61568092+kaenova@users.noreply.github.com>
2023-08-08 12:12:49 +0700

    Time: 45 ms (46.31%), Space: 13.9 MB (15.18%) - LeetHub

---
+++ 1232-check-if-it-is-a-straight-line/1232-check-if-it-is-a-straight-line.py
@@ -0,0 +1,36 @@
+class Solution(object):
+        
+    def checkStraightLine(self, coordinates):
+        """
+        :type coordinates: List[List[int]]
+        :rtype: bool
+        """
+        
+        def slope(c1, c2):
+            dominator = float(c2[1] - c1[1])
+            divider = float(c2[0] - c1[0])
+
+            if (divider == 0):
+                return 0.0, True, False
+            
+            if (dominator == 0):
+                return 0.0, False, True
+            
+            return dominator / divider, False, False
+        
+        i_slope, i_x_axis, i_y_axis = slope(coordinates[0], coordinates[1])
+        for i in range(1, len(coordinates) - 1):
+            c1 = coordinates[i]
+            c2 = coordinates[i + 1]
+            c_slope, c_x_axis, c_y_axis = slope(c1, c2)
+            
+            if i_x_axis and not c_x_axis:
+                return False
+            
+            if i_y_axis and not c_y_axis:
+                return False
+            
+            if c_slope != i_slope:
+                return False
+        
+        return True
