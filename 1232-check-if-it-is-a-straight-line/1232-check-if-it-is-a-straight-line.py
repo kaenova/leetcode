039529ff56e2cb5fa3c3dda3595dc92904d97cfe
Kaenova Mahendra Auditama <61568092+kaenova@users.noreply.github.com>
2023-08-08 12:15:43 +0700

    Time: 36 ms (93.38%), Space: 13.7 MB (96.61%) - LeetHub

--- 1232-check-if-it-is-a-straight-line/1232-check-if-it-is-a-straight-line.py
+++ 1232-check-if-it-is-a-straight-line/1232-check-if-it-is-a-straight-line.py
@@ -1,28 +1,30 @@
 class Solution(object):
+    
+    def slope(self, c1, c2):
+        dominator = float(c2[1] - c1[1])
+        divider = float(c2[0] - c1[0])
+
+        # On X Axis
+        if (divider == 0):
+            return 0.0, True, False
+
+        # On Y Axis
+        if (dominator == 0):
+            return 0.0, False, True
+
+        return dominator / divider, False, False
         
     def checkStraightLine(self, coordinates):
         """
         :type coordinates: List[List[int]]
         :rtype: bool
         """
-        
-        def slope(c1, c2):
-            dominator = float(c2[1] - c1[1])
-            divider = float(c2[0] - c1[0])
-
-            if (divider == 0):
-                return 0.0, True, False
-            
-            if (dominator == 0):
-                return 0.0, False, True
-            
-            return dominator / divider, False, False
 
         c1 = coordinates[0]
-        i_slope, i_x_axis, i_y_axis = slope(c1, coordinates[1])
+        i_slope, i_x_axis, i_y_axis = self.slope(c1, coordinates[1])
         for i in range(1, len(coordinates)):
             c2 = coordinates[i]
-            c_slope, c_x_axis, c_y_axis = slope(c1, c2)
+            c_slope, c_x_axis, c_y_axis = self.slope(c1, c2)
             
             if i_x_axis and not c_x_axis:
                 return False
